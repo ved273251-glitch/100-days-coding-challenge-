670487143bf3741f4c0c1a5bcbc302d93fe5ea34
ved273251-glitch <ved273251@gmail.com>
2026-09-26 18:12:46 +0530

    Add files via upload

---
+++ q95.c
@@ -0,0 +1,27 @@
+// Check if one string is a rotation of another.
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    if (strlen(str1) != strlen(str2)) {
+        printf("Not rotation");
+    }
+    else {
+        strcpy(temp, str1);
+        strcat(temp, str1);
+
+        if (strstr(temp, str2) != NULL)
+            printf("Rotation");
+        else
+            printf("Not rotation");
+    }
+
+    return 0;
+}
+
